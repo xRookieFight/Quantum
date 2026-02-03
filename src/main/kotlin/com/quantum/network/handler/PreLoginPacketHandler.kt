8ca9a103859fc38e2860d5f5ef3b359c8bae5fbf
xRookieFight <me@xrookiefight.com>
2026-02-03 22:02:22 +0300

    feat: Protocol checking on RequestNetworkSettings

--- src/main/kotlin/com/quantum/network/handler/PreLoginPacketHandler.kt
+++ src/main/kotlin/com/quantum/network/handler/PreLoginPacketHandler.kt
@@ -1,25 +1,42 @@
 package com.quantum.network.handler
 
-import org.cloudburstmc.protocol.bedrock.codec.v898.Bedrock_v898
+import com.quantum.network.version.BedrockVersion
 import org.cloudburstmc.protocol.bedrock.data.PacketCompressionAlgorithm
 import org.cloudburstmc.protocol.bedrock.packet.NetworkSettingsPacket
+import org.cloudburstmc.protocol.bedrock.packet.PlayStatusPacket
 import org.cloudburstmc.protocol.bedrock.packet.RequestNetworkSettingsPacket
 import org.cloudburstmc.protocol.common.PacketSignal
 
 class PreLoginPacketHandler : PacketHandler() {
 
-    override fun handle(packet: RequestNetworkSettingsPacket): PacketSignal {
-        val packet = NetworkSettingsPacket().apply {
-            compressionThreshold = 0
-            compressionAlgorithm = PacketCompressionAlgorithm.ZLIB
-        }
+	override fun handle(packet: RequestNetworkSettingsPacket): PacketSignal {
+		val clientVersion = BedrockVersion.fromProtocol(packet.protocolVersion)
+		val latest = BedrockVersion.LATEST
 
-        session.codec = Bedrock_v898.CODEC
+		if (clientVersion == null || !clientVersion.isEqual(latest)) {
+			val playStatus = PlayStatusPacket().apply {
+				status = if (clientVersion != null && clientVersion.isAfter(latest)) {
+					PlayStatusPacket.Status.LOGIN_FAILED_SERVER_OLD
+				} else {
+					PlayStatusPacket.Status.LOGIN_FAILED_CLIENT_OLD
+				}
+			}
 
-        session.sendPacketImmediately(packet)
-        session.setCompression(PacketCompressionAlgorithm.ZLIB)
+			session.sendPacketImmediately(playStatus)
+			session.disconnect()
+			return PacketSignal.HANDLED
+		}
 
-        session.packetHandler = LoginPacketHandler().attach(session, session.server)
-        return PacketSignal.HANDLED
-    }
+		val networkSettings = NetworkSettingsPacket().apply {
+			compressionThreshold = 0
+			compressionAlgorithm = PacketCompressionAlgorithm.ZLIB
+		}
+
+		session.codec = BedrockVersion.LATEST.codec
+		session.sendPacketImmediately(networkSettings)
+		session.setCompression(PacketCompressionAlgorithm.ZLIB)
+
+		session.packetHandler = LoginPacketHandler().attach(session, session.server)
+		return PacketSignal.HANDLED
+	}
 }
--- src/main/kotlin/com/quantum/network/version/BedrockVersion.kt
+++ src/main/kotlin/com/quantum/network/version/BedrockVersion.kt
@@ -1,38 +1,58 @@
 package com.quantum.network.version
 
+import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec
+import org.cloudburstmc.protocol.bedrock.codec.v685.Bedrock_v685
+import org.cloudburstmc.protocol.bedrock.codec.v686.Bedrock_v686
+import org.cloudburstmc.protocol.bedrock.codec.v712.Bedrock_v712
+import org.cloudburstmc.protocol.bedrock.codec.v729.Bedrock_v729
+import org.cloudburstmc.protocol.bedrock.codec.v748.Bedrock_v748
+import org.cloudburstmc.protocol.bedrock.codec.v766.Bedrock_v766
+import org.cloudburstmc.protocol.bedrock.codec.v776.Bedrock_v776
+import org.cloudburstmc.protocol.bedrock.codec.v786.Bedrock_v786
+import org.cloudburstmc.protocol.bedrock.codec.v800.Bedrock_v800
+import org.cloudburstmc.protocol.bedrock.codec.v818.Bedrock_v818
+import org.cloudburstmc.protocol.bedrock.codec.v819.Bedrock_v819
+import org.cloudburstmc.protocol.bedrock.codec.v827.Bedrock_v827
+import org.cloudburstmc.protocol.bedrock.codec.v844.Bedrock_v844
+import org.cloudburstmc.protocol.bedrock.codec.v859.Bedrock_v859
+import org.cloudburstmc.protocol.bedrock.codec.v860.Bedrock_v860
+import org.cloudburstmc.protocol.bedrock.codec.v898.Bedrock_v898
+
 /**
  * Represents supported Minecraft Bedrock Edition versions
  * and their corresponding protocol numbers.
  *
  * @property name Version network name
  * @property protocol Protocol version
+ * @property codec Bedrock codec
  *
  * @author xRookieFight
  * @since 03/02/2026
  */
 data class BedrockVersion(
     val name: String,
-    val protocol: Int
+    val protocol: Int,
+	val codec: BedrockCodec
 ) {
 
     companion object {
-        val V1_21_0   = BedrockVersion("1.21.0", 685)
-        val V1_21_2   = BedrockVersion("1.21.2", 686)
-        val V1_21_20  = BedrockVersion("1.21.20", 712)
-        val V1_21_30  = BedrockVersion("1.21.30", 729)
-        val V1_21_40  = BedrockVersion("1.21.40", 748)
-        val V1_21_50  = BedrockVersion("1.21.50", 766)
-        val V1_21_60  = BedrockVersion("1.21.60", 776)
-        val V1_21_70  = BedrockVersion("1.21.70", 786)
-        val V1_21_80  = BedrockVersion("1.21.80", 800)
-        val V1_21_90  = BedrockVersion("1.21.90", 817)
-        val V1_21_93  = BedrockVersion("1.21.93", 819)
-        val V1_21_100 = BedrockVersion("1.21.100", 827)
-        val V1_21_110 = BedrockVersion("1.21.110", 843)
-        val V1_21_111 = BedrockVersion("1.21.111", 844)
-        val V1_21_120 = BedrockVersion("1.21.120", 859)
-        val V1_21_124 = BedrockVersion("1.21.124", 860)
-        val V1_21_130 = BedrockVersion("1.21.130", 898)
+        val V1_21_0   = BedrockVersion("1.21.0", 685, Bedrock_v685.CODEC)
+        val V1_21_2   = BedrockVersion("1.21.2", 686, Bedrock_v686.CODEC)
+        val V1_21_20  = BedrockVersion("1.21.20", 712, Bedrock_v712.CODEC)
+        val V1_21_30  = BedrockVersion("1.21.30", 729, Bedrock_v729.CODEC)
+        val V1_21_40  = BedrockVersion("1.21.40", 748, Bedrock_v748.CODEC)
+        val V1_21_50  = BedrockVersion("1.21.50", 766, Bedrock_v766.CODEC)
+        val V1_21_60  = BedrockVersion("1.21.60", 776, Bedrock_v776.CODEC)
+        val V1_21_70  = BedrockVersion("1.21.70", 786, Bedrock_v786.CODEC)
+        val V1_21_80  = BedrockVersion("1.21.80", 800, Bedrock_v800.CODEC)
+        val V1_21_90  = BedrockVersion("1.21.90", 818, Bedrock_v818.CODEC)
+        val V1_21_93  = BedrockVersion("1.21.93", 819, Bedrock_v819.CODEC)
+        val V1_21_100 = BedrockVersion("1.21.100", 827, Bedrock_v827.CODEC)
+        val V1_21_110 = BedrockVersion("1.21.110", 843, Bedrock_v844.CODEC)
+        val V1_21_111 = BedrockVersion("1.21.111", 844, Bedrock_v844.CODEC)
+        val V1_21_120 = BedrockVersion("1.21.120", 859, Bedrock_v859.CODEC)
+        val V1_21_124 = BedrockVersion("1.21.124", 860, Bedrock_v860.CODEC)
+        val V1_21_130 = BedrockVersion("1.21.130", 898, Bedrock_v898.CODEC)
 
         val ALL = listOf(
             V1_21_0, V1_21_2, V1_21_20, V1_21_30, V1_21_40,
@@ -61,4 +81,7 @@ data class BedrockVersion(
 
     fun isAfterOrEqual(version: BedrockVersion): Boolean
     = protocol >= version.protocol
+
+	fun isEqual(version: BedrockVersion): Boolean
+	= protocol == version.protocol
 }
